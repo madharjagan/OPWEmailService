12cf05df1ab109e2bb4240ccab32a40c557a65c5
Jagan Kulala Sankar <jagan.sankar@gmail.com>
2018-07-05 20:00:16 -0400

    first commit

---
+++ src/main/java/com/opw/email/client/OPWAmazonSESServiceApplication.java
@@ -0,0 +1,12 @@
+package com.opw.email.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OPWAmazonSESServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OPWAmazonSESServiceApplication.class, args);
+	}
+}
